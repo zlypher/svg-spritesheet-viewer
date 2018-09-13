3723b4ec7a0f69b5c71cd6466b2a955ff7ae1a30
Thomas Prochazka <thomas.prochazka@gmail.com>
2018-09-13 22:21:18 +0200

    Add initial snapshot test for SpritesheetViewer

--- src/__tests__/spritesheet-viewer.spec.tsx
+++ src/__tests__/spritesheet-viewer.spec.tsx
@@ -1,7 +1,17 @@
+import * as React from "react";
 import { SpritesheetViewer } from "../index";
+import renderer from "react-test-renderer";
 
 describe("SpritesheetViewer", () => {
     it("executes a test", () => {
         expect(1).toBe(1);
-    })
+    });
+
+    it("renders correctly and matches the snapshot", () => {
+        const component = renderer.create(
+            <SpritesheetViewer />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
